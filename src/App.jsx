8b5410742314039adac67d816922d7cb351e9326
Sachin Kumar <stdtech7878@gmail.com>
2024-12-14 22:56:32 +0530

    features section has been created

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
+import FeaturesSection from "./components/Features";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar />
       <main className="max-w-[1680px] mx-auto">
         <HeroSection />
+        <FeaturesSection/>
       </main>
     </main>
   );
